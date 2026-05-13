f86e56068cad86c6ce61ad2e021eec3c9e33faad
saidozer034-tech <saidozer034@gmail.com>
2026-05-13 16:28:14 +0300

    feat: add external twitter service

---
+++ src/ExternalTwitterService.java
@@ -0,0 +1,5 @@
+public class ExternalTwitterService {
+    public void postDirectMessage(String msg, String user) {
+        System.out.println("Twitter DM gönderiliyor @" + user + ": " + msg);
+    }
+}
