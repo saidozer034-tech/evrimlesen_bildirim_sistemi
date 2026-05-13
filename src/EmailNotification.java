1860ef07d2582041af007d025f28cebc1626a44d
saidozer034-tech <saidozer034@gmail.com>
2026-05-13 16:24:29 +0300

    refactor: remove accidental file from main to keep baseline clean

--- src/EmailNotification.java
+++
@@ -1,6 +0,0 @@
-public class EmailNotification implements Notification {
-    @Override
-    public void sendMessage(String message, String receiver) {
-        System.out.println("Email gönderiliyor (" + receiver + "): " + message);
-    }
-}
